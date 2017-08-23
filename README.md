145131a31b43d597dc10a2bcf0952ddca939fab9
TURX <turuixuan@foxmail.com>
2017-08-23 23:03:39 +0800

    Rollball Game only for Windows original ver.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# RollBall-Unity
-A roll-ball game demo by Unity
+# [!Unity](https://unity3d.com/profiles/unity3d/themes/unity/images/ui/favicons/favicon.png) RollBall
+A roll-ball game demo by Unity.
+Under GPLv3 License.
---
+++ RollBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text noticeText;
+    public Button quitButton;
+
+    private Rigidbody rb;
+    private int count;
+
+    bool win = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        quitButton.gameObject.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (win == false) rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Collected: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+            noticeText.text = "A great job.";
+            win = true;
+            quitButton.gameObject.SetActive(true);
+        }
+    }
+}
